82811428fb707665f79819831b76756625d31269
Balaji TS <84838739+balaji-ts-18@users.noreply.github.com>
2025-07-23 22:53:43 +0530

    Update D

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./configs/schema.js",
   dbCredentials: {
-    url : 'postgresql://neondb_owner:VcLD1yEXz0YH@ep-round-sea-a5wjdkjj.us-east-2.aws.neon.tech/neondb?sslmode=require'
+    url : NEXT_PUBLIC_DATABASE_CONNECTION_STRING
   }
 });
